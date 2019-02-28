eda565c360f2a8cb142d9b2480cd304d5ee72d6d
Yusril Fahmi Al Faizi <yusrilfahmi09@gmail.com>
2019-02-28 15:59:40 +0700

    Update kontak.php

--- rest_ci/application/controllers/kontak.php
+++ rest_ci/application/controllers/kontak.php
@@ -25,5 +25,48 @@ class Kontak extends REST_Controller {
     }
 
     //Masukan function selanjutnya disini
+    //Mengirim atau menambah data kontak baru
+    function index_post() {
+        $data = array(
+                    'id'           => $this->post('id'),
+                    'nama'          => $this->post('nama'),
+                    'nomor'    => $this->post('nomor'));
+        $insert = $this->db->insert('telepon', $data);
+        if ($insert) {
+            $this->response($data, 200);
+        } else {
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
+
+    //Masukan function selanjutnya disini
+    //Memperbarui data kontak yang telah ada
+    function index_put() {
+        $id = $this->put('id');
+        $data = array(
+                    'id'       => $this->put('id'),
+                    'nama'          => $this->put('nama'),
+                    'nomor'    => $this->put('nomor'));
+        $this->db->where('id', $id);
+        $update = $this->db->update('telepon', $data);
+        if ($update) {
+            $this->response($data, 200);
+        } else {
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
+
+    //Masukan function selanjutnya disini
+    //Menghapus salah satu data kontak
+    function index_delete() {
+        $id = $this->delete('id');
+        $this->db->where('id', $id);
+        $delete = $this->db->delete('telepon');
+        if ($delete) {
+            $this->response(array('status' => 'success'), 201);
+        } else {
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
 }
 ?>
