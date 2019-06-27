569ad72151a13c8dcd47b1a509fbc241b7ba36e5
Yusril Fahmi Al Faizi <44188722+yusrilfahmialfaizi@users.noreply.github.com>
2019-06-27 09:05:24 +0700

    Update Cart.java

--- MutiaraBaru/app/src/main/java/com/example/android/mutiarabaru/Cart.java
+++ MutiaraBaru/app/src/main/java/com/example/android/mutiarabaru/Cart.java
@@ -1,108 +1,38 @@
 package com.example.android.mutiarabaru;
 
-import android.content.Context;
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+public class Cart  {
+	public static String id_barang,nama_barang;
+	public static int qty,harga;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Cart.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Cart#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Cart extends Fragment {
-	// TODO: Rename parameter arguments, choose names that match
-	// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-	private static final String ARG_PARAM1 = "param1";
-	private static final String ARG_PARAM2 = "param2";
-
-	// TODO: Rename and change types of parameters
-	private String mParam1;
-	private String mParam2;
-
-	private OnFragmentInteractionListener mListener;
-
-	public Cart() {
-		// Required empty public constructor
+	public static String getId_barang() {
+		return id_barang;
 	}
 
-	/**
-	 * Use this factory method to create a new instance of
-	 * this fragment using the provided parameters.
-	 *
-	 * @param param1 Parameter 1.
-	 * @param param2 Parameter 2.
-	 * @return A new instance of fragment Cart.
-	 */
-	// TODO: Rename and change types and number of parameters
-	public static Cart newInstance(String param1, String param2) {
-		Cart fragment = new Cart();
-		Bundle args = new Bundle();
-		args.putString(ARG_PARAM1, param1);
-		args.putString(ARG_PARAM2, param2);
-		fragment.setArguments(args);
-		return fragment;
+	public static void setId_barang(String id_barang) {
+		Cart.id_barang = id_barang;
 	}
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		if (getArguments() != null) {
-			mParam1 = getArguments().getString(ARG_PARAM1);
-			mParam2 = getArguments().getString(ARG_PARAM2);
-		}
+	public static String getNama_barang() {
+		return nama_barang;
 	}
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-							 Bundle savedInstanceState) {
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_cart, container, false);
+	public static void setNama_barang(String nama_barang) {
+		Cart.nama_barang = nama_barang;
 	}
 
-	// TODO: Rename method, update argument and hook method into UI event
-	public void onButtonPressed(Uri uri) {
-		if (mListener != null) {
-			mListener.onFragmentInteraction(uri);
-		}
+	public static int getQty() {
+		return qty;
 	}
 
-	@Override
-	public void onAttach(Context context) {
-		super.onAttach(context);
-		if (context instanceof OnFragmentInteractionListener) {
-			mListener = (OnFragmentInteractionListener) context;
-		} else {
-			throw new RuntimeException(context.toString()
-					+ " must implement OnFragmentInteractionListener");
-		}
+	public static void setQty(int qty) {
+		Cart.qty = qty;
 	}
 
-	@Override
-	public void onDetach() {
-		super.onDetach();
-		mListener = null;
+	public static int getHarga() {
+		return harga;
 	}
 
-	/**
-	 * This interface must be implemented by activities that contain this
-	 * fragment to allow an interaction in this fragment to be communicated
-	 * to the activity and potentially other fragments contained in that
-	 * activity.
-	 * <p>
-	 * See the Android Training lesson <a href=
-	 * "http://developer.android.com/training/basics/fragments/communicating.html"
-	 * >Communicating with Other Fragments</a> for more information.
-	 */
-	public interface OnFragmentInteractionListener {
-		// TODO: Update argument type and name
-		void onFragmentInteraction(Uri uri);
+	public static void setHarga(int harga) {
+		Cart.harga = harga;
 	}
 }
